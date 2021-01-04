d15c25de4e00b6dc6711dbe9fca38643ca132d01
isaacdias <isaacardias@gmail.com>
2021-01-04 14:52:52 -0300

    gerando documentação do tipo Exception

--- ByteBank/ByteBank.Modelos/ContaCorrente.cs
+++ ByteBank/ByteBank.Modelos/ContaCorrente.cs
@@ -65,6 +65,12 @@ namespace ByteBank.Modelos
             TaxaOperacao = 30 / TotalDeContasCriadas;
         }
 
+        /// <summary>
+        /// Realiza o saque e atualiza o valor da prorpieddade <see cref="Saldo"/>
+        /// </summary>
+        /// <exception cref="ArgumentException">Exceção lançada quando um valor negativo é utilizado no argumento <paramref name="valor"/>.</exception>
+        /// <exception cref="SaldoInsuficienteException">Exceção lançada quando o valor de <paramref name="valor"/> é maior que o valor da propriedade <see cref="Saldo"/> </exception>
+        /// <param name="valor">Representa o valor do saque e deve ser maioe que <see cref="Saldo"/></param>
         public void Sacar(double valor)
         {
             if (valor < 0)
